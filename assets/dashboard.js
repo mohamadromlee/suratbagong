fe701997f2ae62940379010c729cf56f2db5ed62
mohamad romli <mohamad.romli@pelindo.co.id>
2025-05-09 09:39:02 +0700

    update index, login, dashboard

---
+++ assets/dashboard.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Ini hanyalah contoh. Nantinya, nama pengguna akan diambil dari sesi atau database.
+    const namaPengguna = "Bambang";
+    document.getElementById('nama-pengguna').textContent = namaPengguna;
+});
